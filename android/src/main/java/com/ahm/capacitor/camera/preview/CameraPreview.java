90b619ffe96e451dcf5f6d4c67af9d5fc9520e74
Leyton Blackler <18431950+leytonblackler@users.noreply.github.com>
2024-05-15 15:54:50 +1200

    Test obtaining optimal camera size for Android video record

--- android/src/main/java/com/ahm/capacitor/camera/preview/CameraPreview.java
+++ android/src/main/java/com/ahm/capacitor/camera/preview/CameraPreview.java
@@ -220,7 +220,8 @@ public class CameraPreview extends Plugin implements CameraActivity.CameraPrevie
                     @Override
                     public void run() {
                         // fragment.startRecord(getFilePath(filename), position, width, height, quality, withFlash);
-                        fragment.startRecord(getFilePath(filename), position, width, height, 70, withFlash, maxDuration);
+                        Camera.Size optimalSize = fragment.getOptimalVideoSize();
+                        fragment.startRecord(getFilePath(filename), position, optimalSize.width, optimalSize.height, 70, withFlash, maxDuration);
                     }
                 }
             );
